137371a0326e8c004c59e3c59b1efcb28fff98d3
Roberto <greatswell@gmail.com>
2014-03-28 11:04:19 +0100

    Setting mail modules

---
+++ crypto/mail_imap.py
@@ -0,0 +1,40 @@
+__author__ = 'Roberto'
+
+
+import imaplib
+
+
+host = 'imap.gmail.com'
+
+M = imaplib.IMAP4_SSL(host, 993)
+
+from_field_email = "xxx@yyy.zzz"
+
+user = "username@email.com"
+password = "password"
+
+print(M.login(user, password))
+#for box in M.list()[1]:
+#    print(box.decode('utf-8'))
+
+#typ, data = M.search(None, 'ALL')
+#for num in data[0].split():
+#    typ, data = M.fetch(num, '(RFC822)')
+#    print('Message %s\n%s\n' % (num, data[0][1]))
+#M.close()
+
+print(M.select("INBOX", True))
+
+typ, msgnums = M.search(None, '(FROM "{}")'.format(from_field_email))
+
+#print(typ)
+print(msgnums)
+
+msg = M.fetch("14459", '(RFC822)')
+
+for line in msg[1][0]:
+
+    print(line.decode('utf-8'))
+
+
+print(M.logout())
---
+++ crypto/mail_pop3.py
@@ -0,0 +1,51 @@
+import os
+
+__author__ = 'Roberto'
+
+import poplib
+from email import parser
+
+
+def main():
+
+
+
+    host = 'pop.gmail.com'
+    pop_conn = poplib.POP3_SSL(host)
+    username = 'recent:username@email.com'
+    print(pop_conn.user(username))
+    password = 'password'
+    print(pop_conn.pass_(password))
+
+    #for x in pop_conn.list():
+    #    print(x)
+
+    #msg = pop_conn.retr(307)
+
+    #for x in msg[1]:
+    #    print(x.decode())
+
+    msg_list = pop_conn.list()[1]
+
+    num_messages = len(msg_list)
+    print("Number of messages: {}".format(num_messages))
+
+    for x in msg_list:
+        msg_num = int(x.decode('utf-8').split(" ")[0])
+        msg = pop_conn.top(msg_num, 1)[1]
+        for line in msg:
+            if line.decode('utf-8').startswith("Subject:"):
+                print(line.decode('utf-8'))
+                break
+
+    #for i in range(numMessages):
+
+    #    print(pop_conn.top(i+1, 10)[1])
+
+    #print(pop_conn.top(numMessages-1, 10)[1])
+
+    pop_conn.quit()
+
+
+if __name__ == '__main__':
+    main()
