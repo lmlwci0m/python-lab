74893c37288f459a9d969ee2858f5156b775a76b
Roberto <greatswell@gmail.com>
2014-04-29 13:30:45 +0200

    adding bytee management and transposition

---
+++ utils/notesmgmt.py
@@ -0,0 +1,52 @@
+__author__ = 'roberto'
+
+
+class Note:
+    def __init__(self):
+        self.note = None
+        self.title = None
+        self.tag = None
+        self.parent = None
+
+
+def getNotes(filepath):
+
+    notes = []
+
+    with open(filepath) as f:
+
+        for x in f.read().split("###Title:")[1:]:
+
+            note = Note()
+
+            elems = x.split("###Parent:")
+
+            note.title = elems[0].strip()
+
+            elems_sec = elems[1].split("###Tag:")
+
+            note.parent = elems_sec[0].strip()
+
+            elems_thr = elems_sec[1].split("###Notes:")
+
+            note.tag = elems_thr[0].strip()
+
+            note.notes = elems_thr[1].strip()
+
+            notes.append(note)
+
+    return notes
+
+
+if __name__ == '__main__':
+
+    filepath = "notes.txt"
+
+    notes = getNotes(filepath)
+
+    for note in notes:
+
+        print("------------------------------------------------------------------\n{} [{}]\n{}\n{}".format(note.title,
+                                                                                                           note.parent,
+                                                                                                           note.tag,
+                                                                                                           note.notes))
