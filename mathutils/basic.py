4d4750a7f3a75b138bebb0b033d2423f0625a03a
roberto <greatswell@gmail.com>
2014-08-05 08:48:06 +0200

    add mathutils

---
+++ mathutils/basic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+import sys
+
+def fac(n):
+    """Max recursion: 999"""
+
+    if n == 1 or n == 0: return 1
+    return n * fac(n - 1)
+
+def cb(n, k):
+    return fac(n) / (fac(k) * fac(n - k))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        n = float(sys.argv[1])
+        k = float(sys.argv[2])
+        print(cb(n, k))
+    else:
+        print("plase specify n, k")
